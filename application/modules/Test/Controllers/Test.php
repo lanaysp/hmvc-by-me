b89b97b1bb8c135294047934da39f2cae4e9430f
Lanaysp <lanaysps@gmail.com>
2019-11-18 03:12:56 +0700

    upload HMVC include CI

---
+++ application/modules/Test/Controllers/Test.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Test extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		echo "Hello Word";
+	}
+
+}
+
+/* End of file Test.php */
+/* Location: ./application/controllers/Test.php */
